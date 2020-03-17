a3fdaedd2a7f43b716285cfad0cebc92a4418a24
Emilnurg <emilnurg@ya.ru>
2020-03-17 23:26:07 +0300

    Demo

--- learning_logs/settings.py
+++ learning_logs/settings.py
@@ -126,10 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_PATH = os.path.join(BASE_DIR, 'static')
+#STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    STATIC_PATH,
+    STATIC_ROOT,
 )
 
 
